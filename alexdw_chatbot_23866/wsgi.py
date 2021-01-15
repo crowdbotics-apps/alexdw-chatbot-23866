2c378d099c14e26aa5afe63db427ec2e8ca11c14
Crowdbotics <team@crowdbotics.com>
2021-01-15 18:29:03 +0000

    Initial commit.

---
+++ alexdw_chatbot_23866/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alexdw_chatbot_23866 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alexdw_chatbot_23866.settings')
+
+application = get_wsgi_application()
